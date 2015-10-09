894b94b206d43503b3db9b4f0b24df8b131d55e1
Elizabeth Hale <lizzhale@mac.com>
2015-10-08 19:43:56 -0700

    Add comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// unirest is a lightweight http request client library
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
@@ -6,28 +7,38 @@ var app = express();
 
 app.use(express.static('public'));
 
+// The args argument is an object containing arguments to provide in the query string of the endpoint
 var getFromApi = function(endpoint, args) {
+    // This event emitter will be used to communicate whether retrieving the info was successful
     var emitter = new events.EventEmitter();
+    // The .qs method takes an object consisting of query strings and appends them to the url upon request
     unirest.get('https://api.spotify.com/v1/' + endpoint)
         .qs(args)
         .end(function(response) {
+            // call our own end event after all the data has been received
             if (response.ok) {
+                // attach the response body parsed by Unirest
                 emitter.emit('end', response.body);
             }
             else {
+                // attach the error code returned by Unirest
                 emitter.emit('error', response.code);
             }
         });
     return emitter;
 };
 
+// When a user makes a request to /search/:name, make a request to the Spotify /search endpoint
 app.get('/search/:name', function(req, res) {
+    // remember, first argument is our endpoint, second is our object of query strings
+    // the endpoint is: /search?q=<name>&limit=1&type=artist
     var searchReq = getFromApi('search', {
         q: req.params.name,
         limit: 1,
         type: 'artist'
     });
 
+    // Add listeners to the EventEmiiter returned from getFromApi
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
         res.json(artist);
